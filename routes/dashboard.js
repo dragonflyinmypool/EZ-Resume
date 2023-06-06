d123cbcc1dfa7f5e4a942cd4bfd4d0fa6fe0189b
dragonflyinmypool <jheiney10@gmail.com>
2023-06-06 12:43:02 -0400

    organized, working

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const userDataController = require('../controllers/userDataController');
+const userDataController = require('../controllers/dashboardController');
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/dashboard', auth, userDataController.getDashboard);
-router.post('/dashboard', auth, userDataController.addEntry);
-router.post('/dashboard/basicInfo', auth, userDataController.updateBasicInfo);
-
-router.post(
-  '/dashboard/:section/:itemId',
+router.get('/add-info', auth, userDataController.getDashboard);
+router.post('/add-info', auth, userDataController.addEntry);
+router.post('/basic-info', auth, userDataController.updateBasicInfo);
+router.delete(
+  '/delete-info/:section/:itemId',
   auth,
   userDataController.deleteEntry
 );
-
 router.get('/create-resume', auth, userDataController.getCreateResume);
 router.post('/create-resume', auth, userDataController.postCreateResume);
 
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/userController');
-const userDataController = require('../controllers/userDataController');
-const auth = require('../middlewares/auth');
+const userController = require('../controllers/loginController');
 
 const router = express.Router();
 
@@ -9,18 +7,5 @@ router.get('/', userController.getLoginPage);
 router.post('/', userController.postLogin);
 router.get('/login/:token', userController.getToken);
 router.get('/logout', userController.logout);
-
-router.get('/dashboard', auth, userDataController.getDashboard);
-router.post('/dashboard', auth, userDataController.addEntry);
-router.post('/dashboard/basicInfo', auth, userDataController.updateBasicInfo);
-
-router.post(
-  '/dashboard/:section/:itemId',
-  auth,
-  userDataController.deleteEntry
-);
-
-router.get('/create-resume', auth, userDataController.getCreateResume);
-router.post('/create-resume', auth, userDataController.postCreateResume);
 
 module.exports = router;
